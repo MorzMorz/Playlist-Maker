5b706d74f5179bd778e53d5f3120e7e39cd763fb
MorzMorz <ilja.iwantsov1998@yandex.ru>
2024-09-25 19:45:55 +0300

    Добавлены переходы на соответвущие кнопкам экраны

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,10 +1,13 @@
 package com.practicum.playlistmaker
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.SharedPreferences
+import android.content.res.Configuration
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
-import android.widget.Toast
+
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -13,7 +16,6 @@ import com.google.android.material.switchmaterial.SwitchMaterial
 
 class MainActivity : AppCompatActivity() {
 
-    
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,32 +24,29 @@ class MainActivity : AppCompatActivity() {
 
         val buttonSearch = findViewById<Button>(R.id.button_search)
 
-    buttonSearch.setOnClickListener{
-        Toast.makeText(this@MainActivity, "Нажали на кнопку 'Поиск'!", Toast.LENGTH_SHORT).show()
-    }
+        buttonSearch.setOnClickListener {
+            val displayIntent = Intent(this, SearchActivity::class.java)
+            startActivity(displayIntent)
+        }
+
+
         val buttonMediateka = findViewById<Button>(R.id.button_mediateka)
 
-        val buttonClickListener: View.OnClickListener = object : View.OnClickListener{
-            override fun onClick(v: View?) {
-                Toast.makeText(this@MainActivity, "Нажали на кнопку 'Медиатека'!", Toast.LENGTH_SHORT).show()
-            }
+        buttonMediateka.setOnClickListener {
+            val displayIntent = Intent(this, MediatekaActivity::class.java)
+            startActivity(displayIntent)
         }
-        buttonMediateka.setOnClickListener(buttonClickListener)
+
 
         val buttonSettings = findViewById<Button>(R.id.button_settings)
 
-        buttonSettings.setOnClickListener{
-            Toast.makeText(this@MainActivity, "Нажали на кнопку 'Настройки'!", Toast.LENGTH_SHORT).show()
+        buttonSettings.setOnClickListener {
+            val displayIntent = Intent(this, SettingsActivity::class.java)
+            startActivity(displayIntent)
         }
-
-
     }
+}
 
-
-
-
-
-    }
 
 
 
---
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -0,0 +1,27 @@
+package com.practicum.playlistmaker
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class SettingsActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_settings)
+
+
+        val buttonBack = findViewById<Button>(R.id.button_back)
+
+        buttonBack.setOnClickListener{
+            finish()
+        }
+
+    }
+
+}
