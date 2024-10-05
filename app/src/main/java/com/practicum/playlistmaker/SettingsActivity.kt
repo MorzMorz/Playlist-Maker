7a283888b8328cea7a8ad26d398393c89f74e8f5
MorzMorz <ilja.iwantsov1998@yandex.ru>
2024-10-05 20:45:13 +0300

    Спринт 9. ДЗ. Context и Intent

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -2,13 +2,11 @@ package com.practicum.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class SettingsActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -23,6 +21,59 @@ class SettingsActivity : AppCompatActivity() {
             finish()
         }
 
+        val buttonShare = findViewById<View>(R.id.button_share)
+
+        buttonShare.setOnClickListener{
+            shareApp()
+        }
+
+        val buttonSupport = findViewById<View>(R.id.button_support)
+
+        buttonSupport.setOnClickListener{
+            writeToSupport()
+        }
+
+        val buttonUseragreement = findViewById<View>(R.id.button_userAgreement)
+        buttonUseragreement.setOnClickListener{
+            userAgreement()
+        }
+
     }
+
+    private fun shareApp(){
+        val shareText = getString(R.string.share_text)
+        val sendIntent: Intent = Intent().apply {
+            action = Intent.ACTION_SEND
+            putExtra(Intent.EXTRA_TEXT, shareText)
+            type = "text/plain"
+        }
+        val shareIntent = Intent.createChooser(sendIntent, null)
+        startActivity(shareIntent)
+    }
+
+    private fun writeToSupport(){
+        val email = getString(R.string.email)
+        val subject = getString(R.string.subject)
+        val body = getString(R.string.body)
+
+        val emailIntent = Intent(Intent.ACTION_SENDTO).apply {
+            data = Uri.parse("mailto:")
+            putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+            putExtra(Intent.EXTRA_SUBJECT, subject)
+            putExtra(Intent.EXTRA_TEXT,body)
+        }
+        startActivity(emailIntent)
+    }
+
+    private fun userAgreement(){
+        val userAgreementUrl = getString(R.string.userAgreementUrl)
+        val browserIntent = Intent(Intent.ACTION_VIEW, Uri.parse(userAgreementUrl))
+
+        startActivity(browserIntent)
+    }
+
+
+
+
 
 }
