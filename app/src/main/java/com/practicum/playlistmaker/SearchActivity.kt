02dd621d4641ee146866e2267b0435d7d0f30f8e
MorzMorz <ilja.iwantsov1998@yandex.ru>
2024-10-08 21:37:27 +0300

    Спринт 9. ДЗ. EditText & TextWatcher

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,6 +1,12 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,8 +15,48 @@ import androidx.core.view.WindowInsetsCompat
 class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_search)
 
+        val buttonBack = findViewById<View>(R.id.arrow_back)
+        buttonBack.setOnClickListener {
+            finish()
+        }
+
+        //val linearLayout = findViewById<LinearLayout>(R.id.input_container)
+        val inputEditText = findViewById<EditText>(R.id.input_edit_text)
+        val clearTextButton = findViewById<ImageView>(R.id.clear_text_button)
+
+        clearTextButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+
+        inputEditText.addTextChangedListener(object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                clearTextButton.visibility = clearButtonVisibility(s)
+
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+                // empty
+            }
+        })
+    }
+
+
+    fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
     }
 }
+
+
+
+
+
